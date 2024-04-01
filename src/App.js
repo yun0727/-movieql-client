06f6da891066d874c17c8f92f96fa8eded60ccee
yun <austru00@gmail.com>
2024-04-01 20:37:40 +0900

    0.3 Router

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Movies from "./routes/Movies"
+import Movie from "./routes/Movie"
+
 export default function App(){
-  return <div/>
+  return (
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Movies/>} />
+    <Route path="/movies/:id" element={<Movie/>} />
+  </Routes>
+  </BrowserRouter>)
 }
