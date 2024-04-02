91f81776179a121dcc7299223e1d3152310f149c
yun <austru00@gmail.com>
2024-04-02 20:25:49 +0900

    1.3 useQuery Variables

--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,4 +1,5 @@
 import { useQuery, gql } from "@apollo/client";
+import { Link } from "react-router-dom";
 
 const All_MOVIES = gql`
   query getMovies{
@@ -27,12 +28,8 @@ export default function Movies(){
     <ul>
       <h1>Movies</h1>
       {data.allMovies.map((movie)=>(
-        <li key={movie.id}>{movie.title}</li>
-      ))}
-      <h1>Tweets</h1>
-      {data.allTweets.map((tweet)=>(
-        <li key={tweet.id}>
-          {tweet.text}/by : {tweet.author.fullName}
+        <li key={movie.id}>
+          <Link to={`/movies/${movie.id}`}>{movie.title}</Link> 
         </li>
       ))}
     </ul>
